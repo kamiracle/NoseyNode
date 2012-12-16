f355b06caeb620b26a3bf312fc8a9e4c9107a2ac
Kyle Miracle <adagev2@gmail.com>
2012-12-16 16:36:25 -0500

    cleaned up, added date-utils

--- noseynode.js
+++ noseynode.js
@@ -1,4 +1,6 @@
-function createRecord(addr, url){
+require('date-utils');
+
+function createRecord(addr, url){ 
   var record = {
     "remote_address": addr,
     "url": url
@@ -7,39 +9,36 @@ function createRecord(addr, url){
   return record;
 }
 
-(function() {
-  var PROXY_PORT, httpProxy, server;
-  PROXY_PORT = 8888;
+var PROXY_PORT  = 8888;
+var MONGO_SERVER = "192.168.1.102";
+var httpProxy; 
+var server;
 
 /* Setup MongoDB */
-  var databaseUrl = "mongodb://localhost:27017/noseynode";
-  var collections = ["nnc"];
-  var db = require("mongojs").connect(databaseUrl, collections, function(err,db){
-    if (err) console.log("error");
-  });
+var databaseUrl = "mongodb://" + "192.168.1.102" + ":27017/noseynode";
+var collections = ["nnc"];
+var db = require("mongojs").connect(databaseUrl, collections, function(err,db){
+  if (err) console.log("error");
+});
 
 /* Setup Proxy */
-  httpProxy = require('http-proxy');
-  server = httpProxy.createServer(function(req, res, proxy) {
-    var full_url, location, query;
+httpProxy = require('http-proxy');
+server = httpProxy.createServer(function(req, res, proxy) {
+  var query;
 
-    full_url = "http://" + req.headers['host'] + req.url;
-
-    if (req.headers['host'] != null) {
+  if (req.headers['host'] != null) {
+    db.nnc.save(createRecord(req.connection.remoteAddress, req.url));    // Create and save record 
       
-      db.nnc.save(createRecord(req.connection.remoteAddress, req.url));     
-      proxy.proxyRequest(req, res, {
-        host: req.headers['host'],
-        port: 80
-      });
-    } else {
-      res.end();
-    } 
-  });
+    proxy.proxyRequest(req, res, {    // make proxy request after saving record
+      host: req.headers['host'],
+      port: 80
+    });
+
+  } else { res.end(); } 
+  
+});
 
 /* Start Proxy */
-  server.listen(PROXY_PORT, function() {
-    console.log("HTTP Proxy listening on port " + PROXY_PORT);
-  });
-
-}).call(this);
+server.listen(PROXY_PORT, function() {
+  console.log("HTTP Proxy listening on port " + PROXY_PORT);
+});
