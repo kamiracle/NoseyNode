2fb25665e413e388fa1108e933ca86a45ecaa86e
dothan <dothan@dothan.(none)>
2012-12-16 14:01:52 -0500

    added mongodb

--- noseynode.js
+++ noseynode.js
@@ -1,22 +1,32 @@
+function createRecord(addr, url){
+  var record = {
+    "remote_address": addr,
+    "url": url
+  }
+  console.log(record);
+  return record;
+}
 
 (function() {
-  var PROXY_PORT, PUBLIC_IP, httpProxy, server;
+  var PROXY_PORT, httpProxy, server;
+  PROXY_PORT = 8888;
 
+/* Setup MongoDB */
+  var databaseUrl = "mongodb://localhost:27017/noseynode";
+  var collections = ["nnc"];
+  var db = require("mongojs").connect(databaseUrl, collections, function(err,db){
+    if (err) console.log("error");});
+
+/* Setup Proxy */
   httpProxy = require('http-proxy');
-
-  PROXY_PORT = 8082;
-
-  PUBLIC_IP = '127.0.0.1';
-
   server = httpProxy.createServer(function(req, res, proxy) {
     var full_url, location, query;
 
     full_url = "http://" + req.headers['host'] + req.url;
 
     if (req.headers['host'] != null) {
-
-      console.log(req.connection.remoteAddress);
       
+      db.nnc.save(createRecord(req.connection.remoteAddress, req.url));     
       proxy.proxyRequest(req, res, {
         host: req.headers['host'],
         port: 80
@@ -26,6 +36,7 @@
     } 
   });
 
+/* Start Proxy */
   server.listen(PROXY_PORT, function() {
     console.log("HTTP Proxy listening on port " + PROXY_PORT);
   });
